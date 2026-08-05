e4c1908f70c140562fd2d52b9bf1fd2e7755500b
Blake Piper <blakepiper47@gmail.com>
2026-08-04 22:35:50 -0400

    Fix dock hover reveal delay

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,2 +1,2 @@
 -- Extra autostart processes.
-o.launch_on_start("nwg-dock-hyprland -d -p bottom -a center -i 44 -nolauncher -l top -hl top -hd 20")
+o.launch_on_start("nwg-dock-hyprland -d -p bottom -a center -i 44 -nolauncher -l top -hl top -hd 500")

b8c992f19bd7e6d545cac9bd16084b3febe3a695
Blake Piper <blakepiper47@gmail.com>
2026-08-21 21:21:15 -0400

    Copy XFCE region screenshots to clipboard

--- test/shell.d/standalone-install-test.sh
+++ test/shell.d/standalone-install-test.sh
@@ -93,6 +93,9 @@ grep -Fq 'value="workspace_4_key"' \
 grep -Fq 'value="move_window_workspace_4_key"' \
   "$test_home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml" ||
   fail "XFCE shortcut config includes move-to-workspace bindings"
+grep -Fq 'value="xfce4-screenshooter -r -c"' \
+  "$test_home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml" ||
+  fail "XFCE region screenshots copy directly to the clipboard"
 grep -Fq 'value="exo-open --launch TerminalEmulator"' \
   "$test_home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml" ||
   fail "XFCE shortcut config uses the preferred terminal"
